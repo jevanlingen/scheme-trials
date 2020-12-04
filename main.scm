4272125eda35d201b129e0d33ac34ca2c08e0dec
Jacob van Lingen <jacob.van.lingen@jcore.com>
2020-12-04 17:00:19 +0000

    First functions

--- main.scm
+++ main.scm
@@ -2,3 +2,17 @@
 
 (print ((lambda (x)
   (+ x x)) 22))
+
+(define addWithLambda (lambda (x)
+  (+ x x)))
+
+(define (add x)
+  (+ x x))
+
+(define (addAll . numbers)
+  (apply + numbers))
+
+
+(print (addWithLambda 2223))
+(print (add 2223))
+(print (addAll 2 2 2 2))
